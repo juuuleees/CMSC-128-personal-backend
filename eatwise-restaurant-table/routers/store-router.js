dd7439c8bfee738914156bed6c972bbde626853b
Julianne Ruz <jaruz@up.edu.ph>
2019-04-03 16:51:04 +0800

    Revert "Added addStore() and editStore()."
    This reverts commit e616d83b2bfea4554864f8923c880c39d20a5316.

--- eatwise-restaurant-table/routers/store-router.js
+++ eatwise-restaurant-table/routers/store-router.js
@@ -4,7 +4,5 @@ const StoreController = require('../controllers/store-controller');
 // data encapsulation. The user doesn't really need to see what's going on hihi
 router.get('/store', StoreController.findAllStores);
 router.get('/store/:store_id', StoreController.findStoreById);
-router.post('/store', StoreController.addStore);
 router.post('/store', StoreController.deleteStore);
-router.post('/store/:store_id', StoreController.editStore);
 module.exports = router;
